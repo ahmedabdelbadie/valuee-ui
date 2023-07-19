53f04adce33b90cd4338bff8b77b8f512b09fcdc
ibrahimhamed112 <ibrahim.hamed112@hotmail.com>
2023-07-19 17:25:52 +0300

    fix src of redux

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,1 +1,10 @@
-//Store
+import { configureStore } from "@reduxjs/toolkit";
+import sidebarReducer from "./Slices/Sidebar";
+import configReducer from "./Slices/Config";
+
+export default configureStore({
+  reducer: {
+    sidebar: sidebarReducer,
+    config: configReducer,
+  },
+});
